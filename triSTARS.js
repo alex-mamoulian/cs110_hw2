d93412f330a87ff3e48ba2a2ec9353d103610f8b
alex-mamoulian <mamoulian19@gmail.com>
2016-09-26 01:46:05 +0400

    Delete triSTARS.js

--- triSTARS.js
+++
@@ -1,7 +0,0 @@
-const stars = function(a) {
-if(a===0){return "";} else return "*"+stars(a-1);};
-const blank = function (a) {if (a===0){return "";} else return " " + blank(a-1);};
-const bond = function (a,b) {if(a===0){return "";} else return blank(a)+stars(b)+"\n"+bond(a-1,b+2);};
-const triangleStars = function (a){if(a===0){return "";} else console.log(bond(a,1))
-};
-triangleStars(5);

5f7934535fdf38b0a16f2b09f1d001c83ca03d9b
alex-mamoulian <mamoulian19@gmail.com>
2016-09-26 01:46:28 +0400

    Update Factorial.js

--- Factorial.js
+++ Factorial.js
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-<script>
 const factorial = function(n) 
 {        if(n == 0) {return 1} 
          else {return n * factorial(n - 1);}        }
  
 console.log(factorial(3));
-</script>
-</html>
